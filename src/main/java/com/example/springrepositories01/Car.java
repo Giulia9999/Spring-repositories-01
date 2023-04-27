910581650cd366119cf5029d339121af170ef86f
Giulia <giuliac35@gmail.com>
2023-04-27 19:04:54 +0200

    Add Postman json file

--- src/main/java/com/example/springrepositories01/Car.java
+++ src/main/java/com/example/springrepositories01/Car.java
@@ -13,4 +13,37 @@ public class Car {
     @Column(nullable = false)
     private long serialNumber;
     private double currentPrice;
+
+    public Car(String modelName, long serialNumber, double currentPrice) {
+        this.modelName = modelName;
+        this.serialNumber = serialNumber;
+        this.currentPrice = currentPrice;
+    }
+
+    public Car() {
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public long getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(long serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
 }
